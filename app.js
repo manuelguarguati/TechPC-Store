c3900a4b29ea73f0c90897f0e933fb2052a86e7a
manuelguarguati <manuelguarguati2016@gmail.com>
2025-09-27 15:38:35 -0500

    version 0.1

--- app.js
+++ app.js
@@ -3,24 +3,28 @@ const path = require('path');
 const app = express();
 const Port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+
+
+//  Servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
+//  Rutas principales
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'home.html'));
 });
+
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'login.html'));
-
 });
 
 app.get('/registro', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'registro.html'));
-
 });
 
+//  Iniciar el servidor
 app.listen(Port, () => {
-    console.log(`el servido en  http://localhost:${Port}/home`);
+    console.log(` Servidor en http://localhost:${Port}/home`);
 });
