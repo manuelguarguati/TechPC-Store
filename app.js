db9d1937d7fe7b24b08922d36eb8c3816ecc51a7
manuelguarguati <manuelguarguati2016@gmail.com>
2025-09-28 15:10:55 -0500

    version 2.0

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const Port = 3000;
+const session = require('express-session'); // <-- Esto faltaba
+const sequelize = require('./config/database');
+const User = require('./models/user');
+const authRoutes = require('./routes/auth');
+const bcrypt = require('bcryptjs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(session({ secret:'secret', resave:false, saveUninitialized:true}));
 
 
 //  Servir archivos estáticos
@@ -27,8 +32,9 @@ app.get('/registro', (req, res) => {
 app.get('/carrito', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'carrito.html'));
 });
+app.use('/auth', authRoutes);
 
 //  Iniciar el servidor
 app.listen(Port, () => {
-    console.log(` Servidor en http://localhost:${Port}/home`);
+    console.log(`Servidor en http://localhost:${Port}/home`);
 });
